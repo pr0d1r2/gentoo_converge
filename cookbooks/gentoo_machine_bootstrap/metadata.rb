8f4b0fec69eb7588819b1f4edcab9f0738c054f7
Marcin Nowicki <pr0d1r2@gmail.com>
2017-01-15 10:02:25 +0100

    Add placeholder cookbook to be able to re-use node.json from gentoo_machine_bootstrap

---
+++ cookbooks/gentoo_machine_bootstrap/metadata.rb
@@ -0,0 +1,3 @@
+name 'gentoo_machine_bootstrap'
+version '1.0.0'
+provides 'gentoo_machine_bootstrap'
---
+++ cookbooks/gentoo_machine_bootstrap/recipes/default.rb
@@ -0,0 +1,1 @@
+# NOTE: this only placeholder to be able to re-use node.json from gentoo_machine_bootstrap
