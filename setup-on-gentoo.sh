997af1eede7fb225115abf9944f0897d4e3efffd
Marcin Nowicki <pr0d1r2@gmail.com>
2017-02-11 06:26:48 +0100

    Use local solo config for running in gentoo

--- setup-on-gentoo.sh
+++ setup-on-gentoo.sh
@@ -4,18 +4,4 @@ D_R=`cd \`dirname $0\` ; pwd -P`
 
 berks vendor || exit $?
 
-if [ ! -d /var/chef ]; then
-  mkdir /var/chef || exit $?
-fi
-
-for COOKBOOK_DIR in berks-cookbooks cookbooks
-do
-  rsync -av $D_R/$COOKBOOK_DIR/ /var/chef/cookbooks/ || exit $?
-done
-
-if [ -f $D_R/node.json ]; then
-  cp -rvdp $D_R/node.json /var/chef/node.json || exit $?
-else
-  echo "There is no '$D_R/node.json', aborting ..."
-  exit 8472
-fi
+chef-solo -o "role[host]" -c "$D_R/solo.rb"
