5c72ba356d9a805568fdeee9ef21176129527ef3
Marcin Nowicki <pr0d1r2@gmail.com>
2017-04-14 17:56:13 +0200

    Improve gentoo support. Add gentoo defaults with rebuilding bindist packages

--- cookbooks/gentoo-support/recipes/default.rb
+++ cookbooks/gentoo-support/recipes/default.rb
@@ -1,8 +1,5 @@
+include_recipe 'gentoo-support::portage_defaults'
+
 include_recipe 'gentoo_package_masking'
 
-execute 'touch /etc/portage/package.use/default'
-
 package 'gentoolkit'
-
-package 'eix'
-execute_today 'eix-update'
---
+++ cookbooks/gentoo-support/recipes/portage_defaults.rb
@@ -0,0 +1,20 @@
+execute 'touch /etc/portage/package.use/default'
+execute 'touch /etc/portage/package.mask/default'
+
+{
+  'USE="bindist' => 'USE="-bindist',
+  ' bindist ' => ' -bindist ',
+  ' bindist"' => ' -bindist"'
+}.each do |from_string, to_string|
+  search_and_replace '/etc/portage/make.conf' do
+    from from_string
+    to to_string
+  end
+end
+
+package 'eix'
+execute_today 'eix-update'
+
+execute 'emerge `eix --installed-with-use bindist | grep "^\[I\]" | cut -f 2 -d " " | tac | tr "\n" " "`' do
+  only_if { system('eix --installed-with-use bindist | grep "^\[I\]" | grep -q "[a-z]"') }
+end
