1ea9b613835aacfaca1af962d5fc185b796e5575
Marcin Nowicki <pr0d1r2@gmail.com>
2017-04-15 06:02:42 +0200

    Add XFS filesystem support

--- cookbooks/gentoo-support/recipes/default.rb
+++ cookbooks/gentoo-support/recipes/default.rb
@@ -1,4 +1,5 @@
 include_recipe 'gentoo-support::portage_defaults'
+include_recipe 'gentoo-support::filesystems'
 
 include_recipe 'gentoo_package_masking'
 
---
+++ cookbooks/gentoo-support/recipes/filesystems.rb
@@ -0,0 +1,4 @@
+kernel_module 'CONFIG_XFS_FS'
+kernel_enable 'CONFIG_XFS_QUOTA',
+              'CONFIG_XFS_POSIX_ACL',
+              'CONFIG_XFS_RT'
