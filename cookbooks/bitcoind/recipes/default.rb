7ed528d55e6df4a23d2b265c6f867c157c42c6b6
Marcin Nowicki <pr0d1r2@gmail.com>
2017-04-15 07:29:45 +0200

    Do not enable bitcoind on ZFS systems

--- cookbooks/bitcoind/recipes/default.rb
+++ cookbooks/bitcoind/recipes/default.rb
@@ -14,7 +14,15 @@ template '/etc/bitcoin/bitcoin.conf' do
   variables(node[:bitcoind])
 end
 
+if default[:zfs_pools_require_mountpoints_mounted]
+  # When using zfs_pools usually we put bitcoin data on gzip-9 compressed mountpoint
+  # As zfs_pools is one of the first packages and it allow enforce filesystem mounts
+  # we support this by starting it (but not enabling as after boot there is no ZFS mount)
+  bitcoind_service_ction = [:start]
+else
+  bitcoind_service_ction = [:enable, :start]
+end
 service 'bitcoind' do
-  action [:enable, :start]
+  action bitcoind_service_ction
   subscribes :restart, resources(package: 'net-p2p/bitcoind', template: '/etc/bitcoin/bitcoin.conf')
 end
