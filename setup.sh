27e5702e277f31e9138d164b9117a33cd0bc8ddf
Marcin Nowicki <pr0d1r2@gmail.com>
2017-11-11 05:30:23 +0100

    Conform to shellcheck

--- setup.sh
+++ setup.sh
@@ -1,60 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 
 ADDRESS=$1
 SSH="root@$ADDRESS"
-TODAY=`date +%Y%m%d`
+TODAY=$(date +%Y%m%d)
 
 function run() {
-  ssh $SSH $@ || return $?
+  # shellcheck disable=2068
+  ssh "$SSH" $@ || return $?
 }
 
-cd `dirname $0`
+cd "$(dirname "$0")" || exit $?
 
-run which layman &>/dev/null
-if [ $? -gt 0 ]; then
-  run emerge layman || exit $?
-fi
+run which layman &>/dev/null || run emerge layman || exit $?
 
-run test -f /tmp/.layman-synced-$TODAY
-if [ $? -gt 0 ]; then
+
+if ! (run test -f "/tmp/.layman-synced-$TODAY"); then
   run layman -s ALL || exit $?
-  run touch /tmp/.layman-synced-$TODAY
+  run touch "/tmp/.layman-synced-$TODAY"
 fi
 
-run test -d /var/lib/layman/ssnb
-if [ $? -gt 0 ]; then
-  run layman -a ssnb || exit $?
-fi
+run test -d /var/lib/layman/ssnb || run layman -a ssnb || exit $?
 
-run test -d /etc/portage/package.keywords
-if [ $? -gt 0 ]; then
-  run mkdir /etc/portage/package.keywords
-fi
+run test -d /etc/portage/package.keywords || run mkdir /etc/portage/package.keywords
 
-run test -f /etc/portage/package.keywords/chefdk-omnibus
-if [ $? -gt 0 ]; then
+run test -f /etc/portage/package.keywords/chefdk-omnibus || \
   run 'echo "app-admin/chefdk-omnibus ~amd64" > /etc/portage/package.keywords/chefdk-omnibus'
-fi
 
-run which chef-solo &>/dev/null
-if [ $? -gt 0 ]; then
-  run emerge chefdk-omnibus || exit $?
-fi
+run which chef-solo &>/dev/null || run emerge chefdk-omnibus || exit $?
 
 run touch /etc/portage/package.keywords/default || exit $?
 
-ssh-add -l | grep -q $ADDRESS
-if [ $? -gt 0 ]; then
-  if [ -f ~/.ssh/id_rsa_$ADDRESS ]; then
-    ssh-add ~/.ssh/id_rsa_$ADDRESS
-  fi
-fi
+ssh-add -l | grep -q "$ADDRESS" || test -f "$HOME/.ssh/id_rsa_$ADDRESS" && ssh-add "$HOME/.ssh/id_rsa_$ADDRESS"
 
 berks vendor || exit $?
-knife solo cook root@$ADDRESS || exit  $?
+knife solo cook "root@$ADDRESS" || exit  $?
 
-rsync -av root@$ADDRESS:/root/.ssh/id_rsa.pub cookbooks/ssh-key/files/default/$ADDRESS.pub || exit $?
+rsync -av "root@$ADDRESS:/root/.ssh/id_rsa.pub" "cookbooks/ssh-key/files/default/$ADDRESS.pub" || exit $?
 cd cookbooks/ssh-key/files/default || exit $?
 git commit -a -m "Add key for $ADDRESS" || exit $?
 git push || exit $?
-cd -
+cd - || exit $?
