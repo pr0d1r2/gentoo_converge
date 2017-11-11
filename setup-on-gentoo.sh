d3c4377640c18f16bc084bac65617d48e490f837
Marcin Nowicki <pr0d1r2@gmail.com>
2017-11-11 05:24:34 +0100

    Conform to shellcheck

--- setup-on-gentoo.sh
+++ setup-on-gentoo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-D_R=`cd \`dirname $0\` ; pwd -P`
+D_R=$(cd "$(dirname "$0")" || exit 1 ; pwd -P)
 
-cd $D_R || exit $?
+cd "$D_R" || exit $?
 berks vendor || exit $?
 
 chef-solo -c "$D_R/solo.rb"
